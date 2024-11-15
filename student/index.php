414a9d1b38eddd7d63094679729a72ff50034ca7
Harshini038 <141545251+Harshini038@users.noreply.github.com>
2024-11-15 13:24:04 +0530

    Create index.php

---
+++ student/index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <style>
+        h1{
+            text-align: center;
+        }
+        body{
+            margin:0px;
+        }
+
+        .navbar ul
+        {
+          list-style-type: none;
+          background-color: darkgrey;
+          padding:0px;
+          overflow: hidden;
+
+        }
+
+        .navbar a
+        {
+            color: white;
+            text-decoration: none;
+            padding: 15px;
+            display:block;
+            text-align:center;
+        }
+
+        .navbar li
+        {
+            float:left;
+        }
+
+
+
+    </style>
+</head>
+<body>
+    <h1>Student web interface</h1>
+
+    <nav class="navbar">
+
+    <ul>
+        <li><a href="register.php">Register</li>
+        <li><a href="login.php">Login</li>
+        <li><a href="marks.php">Marks</li>
+        <li><a href="calculate.php">Calculate</li>
+        <li><a href="studmarks.php">Students Marks</li>
+
+
+
+
+    </ul>
+    </nav>
+
+    
+        <h3>
+            This is the home page
+        </h3>
+        <p>SASTRA university welcomes you. Students can use this interface to review their scores.</p>
+    
+    
+</body>
+</html>
